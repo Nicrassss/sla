fae9a7135a11049b47330d8ef5729bfc55a890d3
Nicrassss <126926924+Nicrassss@users.noreply.github.com>
2023-10-23 21:27:38 -0300

    Atualização

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ function somTocaGrilo(){
     document.querySelector("som_tecla-grilo").play();
 }
 document.querySelector("tecla_grilo").onclick=somTocaGrilo;
+const listaDeTeclas = document.querySelectorAll-(".tecla");
+listaDeTeclas[4].onclick = tocaSomGrilo;
